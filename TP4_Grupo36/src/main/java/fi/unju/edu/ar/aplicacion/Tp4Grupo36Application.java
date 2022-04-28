ade52966b12deebccad878eeff1ec3274745383b
JoseAcosta <lpnumb.194@gmail.com>
2022-04-28 00:58:20 -0300

    Inicializacion de Trabajo Practico N4

---
+++ TP4_Grupo36/src/main/java/fi/unju/edu/ar/aplicacion/Tp4Grupo36Application.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo36Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo36Application.class, args);
+	}
+
+}
---
+++ TP4_Grupo36/src/test/java/fi/unju/edu/ar/aplicacion/Tp4Grupo36ApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4Grupo36ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
