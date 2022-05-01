ed07b65ed6de66a31e0d08d9884abbdbb942fe90
arielg <arielg@192.168.0.15>
2022-05-01 19:32:50 -0300

    Creación carpeta layouts, controlador MainController, página Agenda y footer

---
+++ TP4_Grupo36/src/main/java/fi/unju/edu/ar/aplicacion/controller/MainController.java
@@ -0,0 +1,31 @@
+package fi.unju.edu.ar.aplicacion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String getIndex() {		
+		return "index";
+	}
+	
+	@GetMapping("/agenda")
+	public String getAgendaPage() {		
+		return "agenda";
+	}
+
+	@GetMapping("/beneficios")
+	public String getBeneficiosPage() {		
+		return "beneficios";
+	}
+	
+	@GetMapping("/contacto")
+	public String getContactoPage() {		
+		return "contacto";
+	}
+
+}
